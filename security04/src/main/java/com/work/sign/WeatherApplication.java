abd9aea2bae1208d7af084620bbee261281654f2
hjh0915 <623299157@qq.com>
2019-08-03 13:46:53 +0800

    安全登陆第四步

---
+++ security04/src/main/java/com/work/sign/WeatherApplication.java
@@ -0,0 +1,27 @@
+package com.work.sign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.apache.shiro.realm.Realm;
+import org.apache.shiro.realm.SimpleAccountRealm;
+
+@SpringBootApplication
+public class WeatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherApplication.class, args);
+	}
+
+	@Bean
+    public Realm realm() {
+        SimpleAccountRealm realm = new SimpleAccountRealm();
+        realm.addAccount("lonestart", "vespa");
+
+        realm.addRole("schwartz");
+        realm.addRole("goodguy");
+        
+        return realm;
+    }
+
+}
